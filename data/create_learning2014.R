90c7ae908ad39b7cf4451f2fb0059a7c00368ead
GintareBaublyte <gintare.baublyte@helsinki.fi>
2017-02-02 19:07:53 +0200

    Read data, structure, dimensions

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -1,3 +1,9 @@
 #Gintare Baublyte
 #02.02.2017
 #Data wrangling exercise
+
+lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+
+str(lrn14)
+
+dim(lrn14)
