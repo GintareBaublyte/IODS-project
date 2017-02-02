1e2d952117b9db32646560f8c3d46751de8b7610
GintareBaublyte <gintare.baublyte@helsinki.fi>
2017-02-02 19:52:53 +0200

    Analysis dataset created
    Create an analysis dataset with the variables gender, age, attitude,
    deep, stra, surf and points by combining questions in the learning2014
    data. Scale all combination variables to the original scales (by taking
    the mean). Exclude observations where the exam points variable is zero.
    (The data should then have 166 observations and 7 variables)

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -10,3 +10,45 @@ dim(lrn14)
 
 #Data has 183 rows (observations) and 60 columns (variables). Most of the variables are recorded in Likert scale. Gender has numbers 1 and 2 attributed to Female and Male respectively.
 
+library(dplyr)
+
+lrn14$Attitude
+
+lrn14$Attitude / 10
+
+lrn14$attitude <- lrn14$Attitude / 10
+
+# questions related to deep, surface and strategic learning
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+
+# select the columns related to deep learning and create column 'deep' by averaging
+deep_columns <- select(lrn14, one_of(deep_questions))
+lrn14$deep <- rowMeans(deep_columns)
+
+# select the columns related to surface learning and create column 'surf' by averaging
+surface_columns <- select(lrn14, one_of(surface_questions))
+lrn14$surf <- rowMeans(surface_columns)
+
+# select the columns related to strategic learning and create column 'stra' by averaging
+strategic_columns <- select(lrn14, one_of(strategic_questions))
+lrn14$stra <- rowMeans(strategic_columns)
+
+keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
+
+learning2014 <- select(lrn14, one_of(keep_columns))
+
+str(learning2014)
+
+colnames(learning2014)
+
+colnames(learning2014)[2] <- "age"
+
+colnames(learning2014)[7] <- "points"
+
+colnames(learning2014)
+
+learning2014 <- filter(learning2014, points > 0)
+
+str(learning2014)
