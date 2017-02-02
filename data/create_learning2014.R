ef5164a478334e0b2d0e386294c550cfca45c195
GintareBaublyte <gintare.baublyte@helsinki.fi>
2017-02-02 19:15:32 +0200

    Comments for data structure and dimensions

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -7,3 +7,6 @@ lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt
 str(lrn14)
 
 dim(lrn14)
+
+#Data has 183 rows (observations) and 60 columns (variables). Most of the variables are recorded in Likert scale. Gender has numbers 1 and 2 attributed to Female and Male respectively.
+
