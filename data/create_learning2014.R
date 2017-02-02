0174b3d7439789451f95c0b2d8ffb4fb09e19504
GintareBaublyte <gintare.baublyte@helsinki.fi>
2017-02-03 00:59:55 +0200

    added more comments

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -2,9 +2,14 @@
 #02.02.2017
 #Data wrangling exercise
 
+#read data
 lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
 
+#structure of dataset
+
 str(lrn14)
+
+#dimensions of dataset
 
 dim(lrn14)
 
@@ -12,34 +17,39 @@ dim(lrn14)
 
 library(dplyr)
 
+#Attitude is a sum of 10 questions. Here it's scaled back to the original scale of questions.
 lrn14$Attitude
 
 lrn14$Attitude / 10
 
 lrn14$attitude <- lrn14$Attitude / 10
 
-# questions related to deep, surface and strategic learning
+# questions related to deep, surface and strategic learning are combined
 deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D06",  "D15", "D23", "D31")
 surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
 strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
 
-# select the columns related to deep learning and create column 'deep' by averaging
+# columns related to deep learning are selected and new column 'deep' is created by averaging
 deep_columns <- select(lrn14, one_of(deep_questions))
 lrn14$deep <- rowMeans(deep_columns)
 
-# select the columns related to surface learning and create column 'surf' by averaging
+# columns related to surface learning are selected and new column 'surf' is created by averaging
 surface_columns <- select(lrn14, one_of(surface_questions))
 lrn14$surf <- rowMeans(surface_columns)
 
-# select the columns related to strategic learning and create column 'stra' by averaging
+# columns related to strategic learning are selected and new column 'stra' is created by averaging
 strategic_columns <- select(lrn14, one_of(strategic_questions))
 lrn14$stra <- rowMeans(strategic_columns)
+
+#new dataset with just 7 variables is created
 
 keep_columns <- c("gender","Age","attitude", "deep", "stra", "surf", "Points")
 
 learning2014 <- select(lrn14, one_of(keep_columns))
 
 str(learning2014)
+
+#some column names are renamed to match others
 
 colnames(learning2014)
 
@@ -49,9 +59,13 @@ colnames(learning2014)[7] <- "points"
 
 colnames(learning2014)
 
+#those observations that have 0 points are ommited from the final dataset
+
 learning2014 <- filter(learning2014, points > 0)
 
 str(learning2014)
+
+#setting of working directory
 
 setwd("C:/Users/Severi/Documents/Open Data/IODS-project")
 
